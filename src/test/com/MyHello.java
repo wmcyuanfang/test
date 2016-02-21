8f14207be6c10a24617db74a02cbb9170b89e155
305831299 <305831299@qq.com>
2016-02-21 21:20:01 +0800

    first commit
    Change-Id: I8a5d4b40ce096b5f55bb96a82b44c4c21f820f30
    Signed-off-by: 305831299 <305831299@qq.com>

---
+++ src/test/com/MyHello.java
@@ -0,0 +1,10 @@
+package test.com;
+
+public class MyHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/com/domain/MyPerson.java
@@ -0,0 +1,15 @@
+package test.com.domain;
+
+public class MyPerson {
+	String usernanme;
+
+	public String getUsernanme() {
+		return usernanme;
+	}
+
+	public void setUsernanme(String usernanme) {
+		this.usernanme = usernanme;
+	}
+	
+
+}
